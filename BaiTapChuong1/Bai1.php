bff35b9db16e4414045c537840330d827e0079e4
Do Bao Toan <baotoanoffical@gmai.com>
2023-12-31 23:16:38 +0700

    Update Bai 2

--- BaiTapChuong1/Bai1.php
+++ BaiTapChuong1/Bai1.php
@@ -1,4 +1,15 @@
-<?php
-  echo "Hello, World!";
-  echo "Mình đang test code";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bài 1</title>
+</head>
+<body>
+  <?php
+      echo "Good Morning Class !";
+      echo '<font color="#FF0000"> <h3>Good Morning Class !</h3> </font>';
+      echo '<font color="#0000FF"> <h2>Good Morning Class !</h2> </font>';
+  ?>
+</body>
+</html>
---
+++ BaiTapChuong1/Bai2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài 2</title>
+</head>
+<?php
+    $hoten = "";
+    if  ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["txt_Name"])) // Check post not get and check insert by form1
+    {
+        $hoten =$_POST["txt_Name"];
+    }    
+    ?>
+<body>
+    <form name = "f1" method="post" action="Bai2.php">
+        <table width="300" border="1" cellspacing="0" cellpadding="0" align="center" bgcolor="#FFCCFF">
+            <tr align="center">
+                <td colspan="2" bgcolor="#FF66CC">IN LỜI CHÀO</td>
+            </tr>
+            <tr>
+                <td>Nhập họ tên</td>
+                <td><input name="txt_Name" type="text" value="<?php echo $hoten ?>"></td>
+            </tr>
+            <tr>
+                <td colspan="2"> <?php echo "Xin chào bạn: ".$hoten ?></td>
+            </tr>
+            <tr align="center">
+                <td colspan="2"> <input type="submit" name="submit" value="CHÀO"> </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
