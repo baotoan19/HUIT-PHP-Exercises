f2a9fd3a698bf3629b123e5be221a7a08c9216c4
Do Bao Toan <baotoanoffical@gmai.com>
2024-01-02 00:53:28 +0700

    Update Bai 3

---
+++ BaiTapChuong1/Bai3.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tính Diện Tích Hình Chữ Nhật</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        table {
+            width: 600px;
+            margin: 0 auto;
+            border-collapse: collapse;
+            background-color: yellow;
+        }
+
+        th, td {
+            padding: 10px 30px;
+            text-align: left; 
+            font-size: 20px;
+        }
+
+        th {
+            background-color: orange;
+            font-size: 25px;
+            height: 50px;
+            text-align: center;
+        }
+
+        .textbox {
+            height: 30px;
+            width: 80%; 
+            box-sizing: border-box;
+            font-size: 20px;
+        }
+
+        .btn {
+           text-align: center;
+        }
+        .btn input{
+            height: 40px;
+            width: 100px;
+            font-size: 20px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<?php
+    $length = isset($_POST["txt_length"]) ? floatval($_POST["txt_length"]) : 0;
+    $width = isset($_POST["txt_width"]) ? floatval($_POST["txt_width"]) : 0;
+    $acreage = $length * $width;
+?>
+<body>
+    <form name="f1" method="post" action="Bai3.php">
+        <table>
+            <tr>
+                <th colspan="2">TÍNH DIỆN TÍCH HÌNH CHỮ NHẬT</th>
+            </tr>
+            <tr>
+                <td>Length</td>
+                <td><input class="textbox" type="text" name="txt_length" value="<?php echo $length ?>"> </td>
+            </tr>
+            <tr>
+                <td>Width</td>
+                <td> <input class="textbox" type="text" name="txt_width" value="<?php echo $width ?>"> </td>
+            </tr>
+            <tr>
+                <td>Acreage</td>
+                <td> <input class="textbox" type="text" name="txt_acreage" value="<?php echo $acreage ?>" readonly> </td>
+            </tr>
+            <tr >
+                <td class="btn" colspan="2"> <input type="submit" name="submit" value="TÍNH"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
