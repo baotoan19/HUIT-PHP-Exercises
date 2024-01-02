7ac13ce78525d0c020fd0a4a33d61a8470b1fd04
Do Bao Toan <baotoanoffical@gmai.com>
2024-01-02 21:18:28 +0700

    Update Bai 6

--- BaiTapChuong1/Bai6.php
+++ BaiTapChuong1/Bai6.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Đăng Nhập</title>
+    <title>Form String</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -19,7 +19,7 @@
         }
 
         th, td {
-            padding: 10px 40px;
+            padding: 10px 30px;
             text-align: left; 
             font-size: 20px;
         }
@@ -33,7 +33,7 @@
 
         .textbox {
             height: 30px;
-            width: 340%; 
+            width: 250%; 
             box-sizing: border-box;
             font-size: 20px;
             margin-left: -200px;
@@ -48,51 +48,47 @@
             font-size: 20px;
             font-weight: bold;
         }
-        #textbox_output{
-            height: 30px;
-            width: 150%;
-            background-color: aqua;
-            font-size: 20px;
-        }
     </style>
 </head>
 
 <?php
-    $inputString = isset($_POST["txt_input"]) ? $_POST["txt_input"] : "";
-    $searchString = isset($_POST["txt_search"]) ? $_POST["txt_search"] : "";
-    $posstion = 0;
-    if(!empty($inputString) && !empty($searchString))
+    $inputString = isset($_POST["txt_input"]) ? $_POST["txt_input"] :"";
+    $originalString = isset($_POST["txt_original"]) ? $_POST["txt_original"] : "";
+    $replacementString = isset($_POST["txt_replacement"]) ? $_POST["txt_replacement"] : "";
+    $ouputString="";
+    if(!empty($inputString) && !empty($originalString) && !empty($replacementString))
     {
-        $posstion = strpos($inputString,$searchString);
-        if($posstion !== false)
-        {
-            $outputText="Tìm thấy từ '" .$searchString. "' tại vị trí " .$posstion;
-        }
-        else
-        {
-            $outputText="Không tìm thấy từ '" .$searchString. "' trong chuỗi";
-        }
+        $ouputString = str_replace($originalString,$replacementString,$inputString);
+    }
+    else
+    {
+        echo "Vui lòng nhập đầy đủ !!!";
     }
 ?>
 <body>
-    <form name="f1" method="post" action="Bai5.php">
+    <form name="f1" method="post" action="Bai6.php">
         <table>
             <tr>
-                <th colspan="2">TÌM TỪ TRONG CHUỖI</th>
+                <th colspan="2">THAY THẾ CHUỖI</th>
             </tr>
             <tr>
                 <td>Nhập Chuỗi</td>
                 <td><input class="textbox" type="text" name="txt_input" value="<?php echo $inputString  ?>"> </td>
             </tr>
             <tr>
-                <td>Nhập từ</td>
-                <td> <input class="textbox" type="text" name="txt_search" value="<?php echo $searchString ?>"> </td>
-            </tr>
-            <tr >
-                <td class="btn" colspan="2"> <input type="submit" name="submit" value="Tìm Kiếm"></td>
+                <td>Nhập chuỗi gốc</td>
+                <td> <input class="textbox" type="text" name="txt_original" value="<?php echo $originalString ?>"> </td>
             </tr>
             <tr>
-                <td> <input id="textbox_output" type="text" name="" value="<?php echo $outputText ?>" readonly> </td>
+                <td>Nhập chuỗi thay thế</td>
+                <td> <input class="textbox" type="text" name="txt_replacement" value="<?php echo $replacementString ?>"> </td>
+            </tr>
+            <tr >
+                <td class="btn" colspan="2"> <input type="submit" name="submit" value="Thay Thế"></td>
+            </tr>
+            <tr>
+                <td>Kết quả</td>
+                <td> <input style="color: red;" class="textbox" type="text" value="<?php echo $ouputString ?>" readonly> </td>
             </tr>
         </table>
     </form>
